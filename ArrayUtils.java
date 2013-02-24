cd499060ff3b08f08ac100614410ce17da06bb2c
Evgenii Balai <evgenii.balai@ttu.edu>
2013-02-24 11:38:55 -0600

    Add interval queries to segment trees

---
+++ ArrayUtils.java
@@ -0,0 +1,38 @@
+package iensen;
+
+import java.util.Random;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iensen
+ * Date: 2/24/13
+ * Time: 10:15 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArrayUtils
+{
+        public static void shuffle(int[] a) {
+            int n = a.length;
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++) {
+                int change = i + random.nextInt(n - i);
+                swap(a, i, change);
+            }
+        }
+
+        private static void swap(int[] a, int i, int changeIdx) {
+            int temp = a[i];
+            a[i] = a[changeIdx];
+            a[changeIdx] = temp;
+        }
+     public static void reverse(int []a)
+     {
+      for(int i = 0; i < a.length; i++)
+      {
+        int temp = a[i];
+        a[i] = a[a.length - i - 1];
+        a[a.length - i - 1] = temp;
+      }
+     }
+}
---
+++ DataStructures/SegmentTreeOperation.java
@@ -0,0 +1,10 @@
+package iensen.DataStructures;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iensen
+ * Date: 2/24/13
+ * Time: 10:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum SegmentTreeOperation{MIN,MAX,SUM;};
