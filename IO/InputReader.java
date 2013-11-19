ad423bd9d9c5a6e8629aadfdf7a78b3719bf7961
Evgenii Balai <evgenii.balai@ttu.edu>
2013-11-18 21:24:06 -0600

    Add another InputReader

--- IO/InputReader.java
+++ IO/InputReader.java
@@ -1,76 +1,216 @@
-/**
- * Created with IntelliJ IDEA.
- * User: iensen
- * Date: 10/29/12
- * Time: 12:35 PM
- * To change this template use File | Settings | File Templates.
- */
-package iensen.IO;
-import java.awt.*;
-import java.io.BufferedReader;
+package  iensen.IO;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.math.BigInteger;
+import java.util.InputMismatchException;
 
 /**
- * Created with IntelliJ IDEA.
- * User: iensen
- * Date: 10/29/12
- * Time: 10:19 AM
- * To change this template use File | Settings | File Templates.
+ * @author Egor Kulikov (kulikov@devexperts.com)
  */
-
 public class InputReader {
-    StringTokenizer st;
-    BufferedReader in;
-    public InputReader(InputStream ins)
-    {
-        in = new BufferedReader(new InputStreamReader(ins));
+    private boolean finished = false;
+
+    private InputStream stream;
+    private byte[] buf = new byte[1024];
+    private int curChar;
+    private int numChars;
+    private SpaceCharFilter filter;
+
+    public InputReader(InputStream stream) {
+        this.stream = stream;
     }
 
-    public String nextToken()
-    {
-        while(st==null || !st.hasMoreTokens())
-        {
+    public int read() {
+        if (numChars == -1)
+            throw new InputMismatchException();
+        if (curChar >= numChars) {
+            curChar = 0;
             try {
-                st=new StringTokenizer(in.readLine());
+                numChars = stream.read(buf);
             } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                throw new InputMismatchException();
+            }
+            if (numChars <= 0)
+                return -1;
+        }
+        return buf[curChar++];
+    }
+
+    public int peek() {
+        if (numChars == -1)
+            return -1;
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                return -1;
+            }
+            if (numChars <= 0)
+                return -1;
+        }
+        return buf[curChar];
+    }
+
+    public int readInt() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public long readLong() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public String readString() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+    public boolean isSpaceChar(int c) {
+        if (filter != null)
+            return filter.isSpaceChar(c);
+        return isWhitespace(c);
+    }
+
+    public static boolean isWhitespace(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    private String readLine0() {
+        StringBuilder buf = new StringBuilder();
+        int c = read();
+        while (c != '\n' && c != -1) {
+            if (c != '\r')
+                buf.appendCodePoint(c);
+            c = read();
+        }
+        return buf.toString();
+    }
+
+    public String readLine() {
+        String s = readLine0();
+        while (s.trim().length() == 0)
+            s = readLine0();
+        return s;
+    }
+
+    public String readLine(boolean ignoreEmptyLines) {
+        if (ignoreEmptyLines)
+            return readLine();
+        else
+            return readLine0();
+    }
+
+    public BigInteger readBigInteger() {
+        try {
+            return new BigInteger(readString());
+        } catch (NumberFormatException e) {
+            throw new InputMismatchException();
+        }
+    }
+
+    public char readCharacter() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        return (char) c;
+    }
+
+    public double readDouble() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        double res = 0;
+        while (!isSpaceChar(c) && c != '.') {
+            if (c == 'e' || c == 'E')
+                return res * Math.pow(10, readInt());
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res *= 10;
+            res += c - '0';
+            c = read();
+        }
+        if (c == '.') {
+            c = read();
+            double m = 1;
+            while (!isSpaceChar(c)) {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, readInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                m /= 10;
+                res += (c - '0') * m;
+                c = read();
             }
         }
-        return st.nextToken();
-    }
-    public int nextInt()
-    {
-
-        return Integer.parseInt(nextToken());
+        return res * sgn;
     }
 
-    public double nextDouble()
-    {
-        return Double.parseDouble(nextToken());
-    }
-    public long nextLong()
-    {
-       return  Long.parseLong(nextToken());
-    }
-    public String nextLine()  {
-        try {
-            return in.readLine();
-        } catch (IOException e) {
-           // e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            return null;
-        }
-
-    }
-    public char nextChar()  {
-        try {
-            return (char)in.read();
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-        return 0;
+    public boolean isExhausted() {
+        int value;
+        while (isSpaceChar(value = peek()) && value != -1)
+            read();
+        return value == -1;
     }
 
+    public String next() {
+        return readString();
+    }
+
+    public SpaceCharFilter getFilter() {
+        return filter;
+    }
+
+    public void setFilter(SpaceCharFilter filter) {
+        this.filter = filter;
+    }
+
+    public interface SpaceCharFilter {
+        public boolean isSpaceChar(int ch);
+    }
 }
+             
