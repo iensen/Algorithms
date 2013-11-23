48ef5bf9d5f29cc620b3862df9fedc11f15bd909
Evgenii Balai <evgenii.balai@ttu.edu>
2013-11-22 20:37:10 -0600

    Update Input Reader

--- IO/InputReader.java
+++ IO/InputReader.java
@@ -5,212 +5,100 @@ import java.io.InputStream;
 import java.math.BigInteger;
 import java.util.InputMismatchException;
 
-/**
- * @author Egor Kulikov (kulikov@devexperts.com)
- */
 public class InputReader {
-    private boolean finished = false;
 
     private InputStream stream;
-    private byte[] buf = new byte[1024];
-    private int curChar;
-    private int numChars;
-    private SpaceCharFilter filter;
+    int curCharIndex = 0;
+    int charsInBuf = 0;
+     byte buf[] = new byte[16*1024];
 
     public InputReader(InputStream stream) {
         this.stream = stream;
     }
 
-    public int read() {
-        if (numChars == -1)
-            throw new InputMismatchException();
-        if (curChar >= numChars) {
-            curChar = 0;
+
+    public int readChar() {
+
+        if (curCharIndex >= charsInBuf) {
+            curCharIndex = 0;
             try {
-                numChars = stream.read(buf);
+                charsInBuf = stream.read(buf);
             } catch (IOException e) {
                 throw new InputMismatchException();
             }
-            if (numChars <= 0)
+            if (charsInBuf <= 0)
                 return -1;
         }
-        return buf[curChar++];
+        return buf[curCharIndex++];
     }
 
-    public int peek() {
-        if (numChars == -1)
-            return -1;
-        if (curChar >= numChars) {
-            curChar = 0;
-            try {
-                numChars = stream.read(buf);
-            } catch (IOException e) {
-                return -1;
-            }
-            if (numChars <= 0)
-                return -1;
-        }
-        return buf[curChar];
-    }
 
-    public int readInt() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        int sgn = 1;
-        if (c == '-') {
-            sgn = -1;
-            c = read();
-        }
-        int res = 0;
-        do {
-            if (c < '0' || c > '9')
-                throw new InputMismatchException();
-            res *= 10;
-            res += c - '0';
-            c = read();
-        } while (!isSpaceChar(c));
-        return res * sgn;
-    }
-
-    public long readLong() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        int sgn = 1;
-        if (c == '-') {
-            sgn = -1;
-            c = read();
-        }
-        long res = 0;
-        do {
-            if (c < '0' || c > '9')
-                throw new InputMismatchException();
-            res *= 10;
-            res += c - '0';
-            c = read();
-        } while (!isSpaceChar(c));
-        return res * sgn;
-    }
-
-    public String readString() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        StringBuilder res = new StringBuilder();
-        do {
-            res.appendCodePoint(c);
-            c = read();
-        } while (!isSpaceChar(c));
-        return res.toString();
-    }
-
-    public boolean isSpaceChar(int c) {
-        if (filter != null)
-            return filter.isSpaceChar(c);
-        return isWhitespace(c);
-    }
-
-    public static boolean isWhitespace(int c) {
-        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-    }
-
-    private String readLine0() {
+    public String nextLine() {
         StringBuilder buf = new StringBuilder();
-        int c = read();
+        int c = readChar();
         while (c != '\n' && c != -1) {
             if (c != '\r')
                 buf.appendCodePoint(c);
-            c = read();
+                c = readChar();
         }
         return buf.toString();
     }
 
-    public String readLine() {
-        String s = readLine0();
-        while (s.trim().length() == 0)
-            s = readLine0();
-        return s;
-    }
+    public int nextInt() {
 
-    public String readLine(boolean ignoreEmptyLines) {
-        if (ignoreEmptyLines)
-            return readLine();
-        else
-            return readLine0();
-    }
+        int c;
+        do {
+            c = readChar();
+        }while(isWhitespace(c));
 
-    public BigInteger readBigInteger() {
-        try {
-            return new BigInteger(readString());
-        } catch (NumberFormatException e) {
-            throw new InputMismatchException();
-        }
-    }
-
-    public char readCharacter() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        return (char) c;
-    }
-
-    public double readDouble() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        int sgn = 1;
+        int sign = 1;
         if (c == '-') {
-            sgn = -1;
-            c = read();
+            c = readChar();
+            sign = -1;
+
         }
-        double res = 0;
-        while (!isSpaceChar(c) && c != '.') {
-            if (c == 'e' || c == 'E')
-                return res * Math.pow(10, readInt());
-            if (c < '0' || c > '9')
-                throw new InputMismatchException();
+        int res = 0;
+        do {
             res *= 10;
             res += c - '0';
-            c = read();
-        }
-        if (c == '.') {
-            c = read();
-            double m = 1;
-            while (!isSpaceChar(c)) {
-                if (c == 'e' || c == 'E')
-                    return res * Math.pow(10, readInt());
-                if (c < '0' || c > '9')
-                    throw new InputMismatchException();
-                m /= 10;
-                res += (c - '0') * m;
-                c = read();
-            }
-        }
-        return res * sgn;
+            c = readChar();
+        } while (!isWhitespace(c) && c!=-1);
+        return sign * res;
     }
 
-    public boolean isExhausted() {
-        int value;
-        while (isSpaceChar(value = peek()) && value != -1)
-            read();
-        return value == -1;
+    public String nextToken() {
+        int c;
+        do {
+            c = readChar();
+        }while(isWhitespace(c));
+
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = readChar();
+        } while (!isWhitespace(c) && c!=-1);
+        return res.toString();
     }
+
+    /*
+    * For the purpose of compatibilty with Chelper Chrome extension
+     */
 
     public String next() {
-        return readString();
+        return nextToken();
     }
 
-    public SpaceCharFilter getFilter() {
-        return filter;
+
+    public double nextDouble() {
+        return Double.parseDouble(nextToken());
     }
 
-    public void setFilter(SpaceCharFilter filter) {
-        this.filter = filter;
-    }
 
-    public interface SpaceCharFilter {
-        public boolean isSpaceChar(int ch);
+
+    private boolean isWhitespace(int c) {
+        return  c==' ' || c=='\n'|| c=='\r' || c=='\t';
     }
 }
-             
+
+
+
